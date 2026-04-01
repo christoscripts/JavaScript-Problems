45bb5df5959c2adc95e117bf43055d5cd23a9bcf
Christo Mannic <christomannic0@gmail.com>
2026-04-02 00:07:52 +0500

    Implement functions for grade statistics

--- script.js
+++ script.js
@@ -51,3 +51,125 @@ function getYear(years, searchYear) {
 // Sample usage - do not modify
 console.log(getYear([2019, 2020, 2021], 2020)); // 2020
 console.log(getYear([2019, 2020, 2021], 1990)); // undefined
+
+
+// Problem : Complete the function getOddYears such that it returns all the years that are odd from the years parameter it receives.
+
+/**
+ * @param {number[]} years
+ */
+function getOddYears(years) {
+    const result = years.filter(function(year) {
+        return !(year%2 == 0)
+    })
+    return result
+}
+
+// Sample usage - do not modify
+console.log(getOddYears([2019, 2020, 2021])); // [2019, 2021]
+console.log(getOddYears([2000, 2015, 2018, 2020])); // [2015]
+
+
+// Problem : Complete the function isAppUsed such that it returns true when the app parameter it receives exists in the apps parameter, and false otherwise.
+
+/**
+ * @param {string[]} apps
+ * @param {string} app
+ */
+function isAppUsed(apps, app) {
+    return apps.includes(app)
+}
+
+// Sample usage - do not modify
+console.log(isAppUsed(["Twitter", "Calculator"], "Calculator")); // true
+console.log(isAppUsed(["Clock", "Calculator"], "Safari")); // false
+
+// Problem : Complete the function getStringSizes such that it returns an array of the number of characters for every string it receives in the strings parameter.
+
+/**
+ * @param {string[]} strings
+ */
+function getStringSizes(strings) {
+    const result = strings.map(function(string) {
+        return string.length
+    })
+    return result
+}
+
+// Sample usage - do not modify
+console.log(getStringSizes(["a", "abc"])); // [1, 3]
+console.log(getStringSizes(["Sam", "Alex", "Charlie"])); // [3, 4, 7]
+console.log(getStringSizes(["Hello", "Blue"])); // [5, 4]
+
+
+// Problem : Classroom Project I 
+
+// This is a classroom project! You can enter grades in the browser tab and add them to the app, which will show you various statistics about the classroom. For example, you will see the average grade, all the failing grades, etc.
+
+// Each function has a comment describing what it should return, but you can also find them below:
+
+// getNumberOfGrades should return the number of grades.
+// getSumGrades should return the sum of all the grades.
+// getAverageValue should return the average value of all grades (sum of all grades divided by the total number of grades).
+// getPassingGrades should return all passing grades (10 and above).
+// getFailingGrades should return all failing grades (9 and below).
+// getRaisedGrades should return all the grades raised by 1 (grades should not exceed 20).
+
+
+/** @param {number[]} grades */
+export function getNumberOfGrades(grades) {
+    // TODO: return the number of grades
+    return grades.length
+}
+
+/** @param {number[]} grades */
+export function getSumGrades(grades) {
+    // TODO: return the sum of all the grades
+    let sum = 0
+    grades.forEach(function(grade) {
+        sum += grade
+    })
+    return sum
+}
+
+/** @param {number[]} grades */
+export function getAverageValue(grades) {
+    // TODO: return the average value of all grades (sum of all grades divided by the total number of grades)
+    let sum = 0
+    grades.forEach(function(grade) {
+        sum += grade
+    })
+    let result = sum/grades.length
+    return result
+}
+
+/** @param {number[]} grades */
+export function getPassingGrades(grades) {
+    // TODO: return all passing grades (10 and above)
+    let result = grades.filter(function(grade) {
+        return grade >= 10
+    })
+    return result
+}
+
+/** @param {number[]} grades */
+export function getFailingGrades(grades) {
+    // TODO: return all failing grades (9 and below)
+    let result = grades.filter(function(grade) {
+        return grade < 10
+    })
+    return result
+}
+
+/** @param {number[]} grades */
+export function getRaisedGrades(grades) {
+    // TODO: return all the grades raised by 1 (no grade should exceed 20)
+    let result = grades.map(function(grade) {
+        grade++
+        if(grade > 20) {
+            grade = 20
+        }  
+        return grade
+    })
+    return result
+}
