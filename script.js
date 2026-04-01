0c2b55b51ee75e58454ec7159850d84d5c2151d2
Christo Mannic <christomannic0@gmail.com>
2026-04-01 23:15:36 +0500

    Add function to get freezing temperatures from array
    Implemented getFreezingTemperatures function to filter out temperatures below 0.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Problem : Complete the function getPositiveTemperatures such that it returns an array containing the positive temperatures (the temperatures that are below 0).
+
 /**
  * @param {number[]} temperatures
  */
@@ -14,3 +16,20 @@ function getPositiveTemperatures(temperatures) {
 // Sample usage - do not modify
 console.log(getPositiveTemperatures([-5, 12, 3])); // [12, 3]
 console.log(getPositiveTemperatures([1, -3, -2, 4, 10])); // [1, 4, 10]
+
+
+// Problem : Complete the function getFreezingTemperatures such that it returns an array containing the freezing temperatures (the temperatures that are below 0).
+
+/**
+ * @param {number[]} temperatures
+ */
+function getFreezingTemperatures(temperatures) {
+    const temp = temperatures.filter(function(temperature) {
+        return temperature < 0
+    })
+    return temp
+}
+
+// Sample usage - do not modify
+console.log(getFreezingTemperatures([-5, 12, 3])); // [-5]
+console.log(getFreezingTemperatures([1, -3, -2, 4, 10])); // [-3, -2]
