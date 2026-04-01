54bea77e2e703238e99f72879035d626c4aa2148
Christo Mannic <christomannic0@gmail.com>
2026-04-01 23:12:25 +0500

    Implement getPositiveTemperatures function
    Add function to filter positive temperatures from an array

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+/**
+ * @param {number[]} temperatures
+ */
+function getPositiveTemperatures(temperatures) {
+    
+    const temp = temperatures.filter(function(temperature) {
+        return temperature > 0
+    })
+    return temp
+}
 
+// Sample usage - do not modify
+console.log(getPositiveTemperatures([-5, 12, 3])); // [12, 3]
+console.log(getPositiveTemperatures([1, -3, -2, 4, 10])); // [1, 4, 10]
